b4feb0e409ebf3307c805b2c10b08db27a6df20d
alenslhi <silalahirichardvp@gmail.com>
2026-05-28 00:42:58 +0800

    first commit

--- deploy_spendora/.next/server/app/api/auth/[...nextauth]/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/[...nextauth]/route.js")
-R.c("server/chunks/[externals]__0_97j-~._.js")
-R.c("server/chunks/node_modules_0xd69n1._.js")
-R.c("server/chunks/[root-of-the-server]__0p3fo6l._.js")
-R.c("server/chunks/_next-internal_server_app_api_auth_[___nextauth]_route_actions_04_0y9h.js")
-R.m(3413)
-module.exports=R.m(3413).exports
